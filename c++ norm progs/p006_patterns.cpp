fa4313931c07c7bf50f0d9b4a4b7809603c9c142
Rajveer Singh <rajveercode@hotmail.com>
2026-06-06 17:40:08 +0530

    Add Christmas Tree pattern implementation and enhance ASCII character printing

--- c++ norm progs/p006_patterns.cpp
+++ c++ norm progs/p006_patterns.cpp
@@ -34,6 +34,33 @@ int main()
         }
         
         cout<<endl;
-    }
-}
+    }cout<<endl;
+    //Christmas Tree Pattern:
+    for (int r = 1; r<=row; r++){
+    int q = r + 64;
+    for (int s = 1; s <= (row - r); s++) {
+            cout << " ";
+        }
+        for (int c = 1; c<=r; c++){
+            cout<<static_cast<char>(q)<<" ";
+        }cout<<endl;
+    }cout<<endl;
+    for (int r = 1; r<=row; r++){
+    int q = r + 64;
+    for (int s = 1; s <= (row - r)*2; s++) {
+            cout << " ";
+        }
+        for (int c = 1; c<=r; c++){
+            cout<<static_cast<char>(q)<<" ";
+        }cout<<endl;
+     }cout<<endl;
+    for (int r = 1; r<=row; r++){
+    for (int s = 1; s <= (row - r)*2; s++) {
+            cout << " ";
+        }
+        for (int c = 1; c<=r; c++){
+            int q = c + 64;
+            cout<<static_cast<char>(q)<<" ";
+        }cout<<endl;
+}}
 //learnt how to print the ASCII value of a given number by static_cast
