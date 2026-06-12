42a2771f3b05744c28b70f5e333f21716037189d
Rajveer Singh <rajveercode@hotmail.com>
2026-06-12 23:25:29 +0530

    Add p011_switch.cpp

---
+++ c++ norm progs/p011_switch.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+#include <vector>
+using namespace std;
+
+int main(){
+int i;
+cout<<"Enter a number: ";
+cin>>i;
+switch(i){
+    case 1 :
+    cout<<"Alexander";
+    break;
+
+    case 2:
+    cout<<"Adolf Hitler";
+    break;
+
+    default:
+    cout<<"Nepoleon";
+    break;
+}
+}
