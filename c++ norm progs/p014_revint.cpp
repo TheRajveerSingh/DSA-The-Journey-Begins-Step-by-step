3d645899e527d32bab12892258aa898b445c337a
Rajveer Singh <rajveercode@hotmail.com>
2026-06-14 20:57:41 +0530

    Add reverse integer program and update executable

---
+++ c++ norm progs/p014_revint.cpp
@@ -0,0 +1,31 @@
+#include <iostream>
+#include <vector>
+#include <climits> //Required for int_max and int_min
+using namespace std;
+class Solution {
+public:
+    int reverse(int x) {
+        int ans = 0, rem ;
+        while(x!=0){
+            rem = x%10;
+            x/=10;
+            if(ans>INT_MAX/10 || ans<INT_MIN/10)
+            {return 0;}
+            ans = (ans*10)+rem;}
+        
+        return ans; }
+    };
+int main() {
+    Solution solver; //solver is an object/instance of Solution function to use it
+    int inputNumber;
+
+    cout << "Enter any number, -ve/+ve: ";
+    cin >> inputNumber;
+
+    int result = solver.reverse(inputNumber);
+    cout << "Reversed number: " << result << endl;
+
+    return 0;
+}
+//So basically an object gives access to use a class, and the functions inside it
+//But, only 'public' functions can be accessed by the object, not private ones.
