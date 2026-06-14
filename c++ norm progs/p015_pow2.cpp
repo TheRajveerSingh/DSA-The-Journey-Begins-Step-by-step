4a861820c1a082eda8fb2d92e150ee9ad6616123
Rajveer Singh <rajveercode@hotmail.com>
2026-06-14 22:01:27 +0530

    Add alternative approach for checking powers of 2 in p015_pow2.cpp

--- c++ norm progs/p015_pow2.cpp
+++ c++ norm progs/p015_pow2.cpp
@@ -18,3 +18,28 @@ int main(){
     cout << num << " does not come in the power of 2" << endl;
     return 0;
 }
+//Just incase if we want to try another approach:
+// A property of powers of 2: (num & (num - 1)) is always 0
+/*ne more approach can be first converting the number into binary,
+and then seeing that if its binary form is 
+to the power of 10 or not (Ex: 10, 100, 1000, 1000..)*/
+/*
+int num2;
+    cout << "Enter a number: ";
+    cin >> num2;
+
+    // Numbers less than or equal to 0 cannot be powers of 2
+    if (num2 <= 0) {
+        cout << num2 << " does not come in the power of 2" << endl;
+        return 0;
+    }
+
+    // A property of powers of 2: (num & (num - 1)) is always 0
+    if ((num2 & (num2 - 1)) == 0) {
+        cout << num2 << " comes in the power of 2" << endl;
+    } else {
+        cout << num2 << " does not come in the power of 2" << endl;
+    }
+
+    return 0;
+*/
