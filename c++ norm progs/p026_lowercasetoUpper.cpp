8e41d4436a590a226ec2eaedeaca705f0475fb32
Rajveer Singh <rajveercode@hotmail.com>
2026-07-11 16:15:13 +0530

    Add implementation for converting lowercase letters to uppercase

---
+++ c++ norm progs/p026_lowercasetoUpper.cpp
@@ -0,0 +1,35 @@
+#include <iostream>
+#include <vector>
+#include <string>
+using namespace std;
+
+char lowertoUpper(char lower){
+    char upper = lower - 'a' + 'A';
+    return upper;
+}
+
+string lowertoUpper(string word){
+    for (int i=0; i<word.length(); i++){
+        word[i] = word[i] - 'a' + 'A'; 
+    }
+    return word; 
+}
+
+int main(){
+    cout<<"Enter a lower case alphabet: ";
+    char lower;
+    cin>>lower;
+    cout<<lowertoUpper(lower)<<endl;
+    cout<<"Enter a word in lower case: ";
+    string word;
+    cin>>word;
+    cout<<lowertoUpper(word)<<endl;
+    return 0;
+}
+/*
+Result:
+Enter a lower case alphabet: j
+J
+Enter a word in lower case: everyone
+EVERYONE
+*/
