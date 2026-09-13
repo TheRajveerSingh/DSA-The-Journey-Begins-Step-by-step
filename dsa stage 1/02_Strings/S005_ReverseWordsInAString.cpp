47857db650a14cc3813722f280bb6188014eb08e
Rajveer Singh <rajveercode@hotmail.com>
2026-09-13 15:50:06 +0530

    Implemented reverse words functionality in a string using stringstream and vector. Added strategy comments and a main function for testing.

--- dsa stage 1/02_Strings/S005_ReverseWordsInAString.cpp
+++ dsa stage 1/02_Strings/S005_ReverseWordsInAString.cpp
@@ -22,4 +22,51 @@ Explanation: You need to reduce multiple spaces between two words to a single sp
 //.............................................................................................................
 /*
 Stratergy:
+To read the given string.
+Then to seperate each word from the string (by using stringstream).
+Storing each word in a vector array.
+Lastly printing the words in that array backwards.
+*/
+#include <iostream>
+#include <vector>
+#include <sstream>
+using namespace std;
+class Solution{
+    public:
+    string reverseWords(string s){
+        stringstream ss(s); //To capture individual words from string s
+        string word;        //individual 'word' from ss to be pushed into a vector array
+        vector<string> words;  //To push 'word' from ss to a vector array 'words'
+        string ans;         //Used to store words while traversing the vector from backwards
+
+        //Extracting words - For every word being given out by ss for string s
+        while(ss >> word){
+            words.push_back(word);
+        }
+
+        //traversing and storing words in 'ans' from the back of the array
+        for (int i = words.size() - 1; i>=0; i--){
+            ans += words[i];
+
+            //So that after every word added, a space can also be there
+            if (i != 0) {
+                ans += " ";
+            }
+        }
+        return ans;
+    }
+};
+int main(){
+    Solution solver;
+    string s;
+    cout<<"Enter a string: ";
+    getline(cin, s);
+    cout<<solver.reverseWords(s);
+}
+/*
+Result:
+Enter a string: The Sky is Blue
+Blue is Sky The
+Enter a string: Why   is    there so  confusing           spaces  randomly 
+randomly spaces confusing so there is Why
 */
