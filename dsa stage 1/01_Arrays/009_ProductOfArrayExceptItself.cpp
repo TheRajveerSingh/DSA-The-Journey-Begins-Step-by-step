e2fa9b0db490c307ae02f602f23b555326d1bf62
Rajveer Singh <rajveercode@hotmail.com>
2026-09-19 13:32:27 +0530

    Refactored and Completed Product of Array Except Self algorithm and code. This took O(n2) time complexity.

--- dsa stage 1/01_Arrays/009_ProductOfArrayExceptItself.cpp
+++ dsa stage 1/01_Arrays/009_ProductOfArrayExceptItself.cpp
@@ -12,23 +12,71 @@ Example 2:
 Input: nums = [-1,1,0,-3,3]
 Output: [0,0,9,0,0]
 */
-vector<int> productExceptSelf(vector<int>& nums) {
-    int n = nums.size();
-    vector<int> answer(n, 1);
+//................................................................................................
+/*
+Stratergy:
+First I thought of just multipling all the elements of the array at once, and then dividing them with each element in the array.
+But this stratergy fails when we have '0' in the array. Additionally, the question explicity mentions not to use the division operator.
+So,
+what we do is, 
+that for each element in the array,
+we multiply all the elements in the right side,
+and then multiply all the elements in the left side,
+and then multiply both of those results (left * right), and place the answer in that index's place itself, right?
+*/
+#include <iostream>
+#include <vector>
+using namespace std;
+class Solution{
+    public:
+    vector<int> productExceptSelf(vector<int>& nums){
+        int n = nums.size();
+        vector<int> answer(n);
 
-    // Left pass
-    int left = 1;
-    for (int i = 0; i < n; i++) {
-        answer[i] = left;
-        left *= nums[i];
+        for (int i=0; i<n; i++){   //For each 'i' in the array nums
+
+            //Calculating left
+            int left = 1;
+            for (int j=0; j<i; j++){
+                left *= nums[j];
+            }
+            //Calculating right
+            int right = 1;
+            for (int j=i+1; j<n; j++){
+                right *= nums[j];
+            }
+
+            answer[i] = left * right;  //placing the value in that very index location in the new array
+        }
+        return answer;
     }
-
-    // Right pass
-    int right = 1;
-    for (int i = n - 1; i >= 0; i--) {
-        answer[i] *= right;
-        right *= nums[i];
+};
+int main(){
+    int n, m;
+    vector<int> nums;
+    cout<<"Enter the size of the array: ";
+    cin>>n;
+    cout<<"Enter the values of the array: ";
+    for (int k=0; k<n; k++){
+        cin>>m;
+        nums.push_back(m);
     }
-
-    return answer;
+    Solution solver;
+    vector<int> answer = solver.productExceptSelf(nums);  //Remember that when we need to print the elements of a returned vector, we have to store it again in a vector and traverse it to print its elements.
+    for(int t : answer){
+        cout<<t<<" ";
+    }
+    return 0;
 }
+/*
+Results:
+Enter the size of the array: 5
+Enter the values of the array: 1 2 3 4 5
+120 60 40 30 24 
+Enter the size of the array: 4
+Enter the values of the array: 1 2 3 4
+24 12 8 6 
+Enter the size of the array: 5
+Enter the values of the array: -1 1 0 -3 3
+0 0 9 0 0 
+*/
